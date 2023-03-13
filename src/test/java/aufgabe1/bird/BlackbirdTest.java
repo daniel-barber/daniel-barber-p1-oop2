38cefd920314db941445630e5380091c8c515b13
daniel.barber <daniel.barber@students.fhnw.ch>
2023-03-13 13:25:31 +0100

    BlackbirdTest done

--- src/test/java/aufgabe1/bird/BlackbirdTest.java
+++ src/test/java/aufgabe1/bird/BlackbirdTest.java
@@ -1,7 +1,23 @@
 package aufgabe1.bird;
 
+import aufgabe1.food.Nut;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class BlackbirdTest {
 
     // TODO Test für `Blackbird` schreiben (keine Logik von `Bird` testen!)
+
+    @Test
+    public void testHasPreferredFood(){
+
+        //given
+        //when
+        Blackbird blackbird = new Blackbird();
+        //then
+        assertEquals(2, blackbird.getPreferredFoods().size());
+    }
 
 }
